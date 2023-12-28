1fa210ba169419bbcace100c49823687084bc0a5
Kumar <150427325+Kumar130606@users.noreply.github.com>
2023-12-28 22:00:15 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,125 +1,121 @@
-let board; 
-let context; 
- 
-// Dino 
-let dino = { 
-  x: 50, 
-  y: 180, // Place Dino at the bottom 
-  width: 20, 
-  height: 20, 
-  color: '#666', 
-}; 
- 
-let obstacles = []; 
-let gravity = 0.8; 
-let isJumping = false; 
-let jumpVelocity; 
- 
-let spawnChance = 0.02; 
-let score = 0; 
- 
-// Initialize the board and context 
-window.onload = function () { 
-  board = document.getElementById('board'); 
-  context = board.getContext('2d'); 
-  board.width = 600; 
-  board.height = 200; 
- 
-  document.addEventListener('keydown', jump); 
- 
-  // Request first frame 
-  requestAnimationFrame(animate); 
-}; 
- 
- 
- 
-// Animation loop 
-function animate() { 
-  update(); 
-  requestAnimationFrame(animate); 
-} 
- 
-function update() { 
-  context.clearRect(0, 0, board.width, board.height); 
- 
-  spawnChance += 0.001; 
- 
-  if (Math.random() < spawnChance) { 
-    spawnObstacle(); 
-  } 
- 
-  // Handle jumping with gravity 
-  if (isJumping) { 
-    dino.y -= jumpVelocity; 
-    jumpVelocity -= gravity; 
-    if (dino.y > 180) { 
-      isJumping = false; 
-      dino.y = 180; 
-      jumpVelocity = 0; 
-    } 
-  } 
- 
-  // Move and draw obstacles 
-  obstacles.forEach(function (obs) { 
-    obs.x -= 5; 
-    context.fillStyle = obs.color; 
-    context.globalAlpha = 0.4; // Reduce opacity further 
-    context.fillRect(obs.x, obs.y, obs.size, obs.size); 
-    context.globalAlpha = 1; // Reset opacity 
-  }); 
- 
-  // Collision detection 
-  obstacles.forEach(function (obs) { 
-    if ( 
-      obs.x < dino.x + dino.width && 
-      obs.x + obs.size > dino.x && 
-      obs.y < dino.y + dino.height && 
-      obs.y + obs.size > dino.y 
-    ) { 
-      gameOver(); 
-    } 
-  }); 
- 
-  // Draw dino 
-  context.fillStyle = dino.color; 
-  context.fillRect(dino.x, dino.y, dino.width, dino.height); 
- 
-  // Draw score 
-  context.fillStyle = '#000'; 
-  context.font = '20px Arial'; 
-  context.fillText('Score: ' + score, 50, 50); 
- 
-  score++; // Increment score 
-} 
- 
-function spawnObstacle() { 
-  let obsSize = 15; // Square size 
-  let obsSpacingMultiplier = 4; // Adjust this multiplier for initial spacing 
-  let obsSpacing = dino.width * obsSpacingMultiplier; 
- 
-  // Increase the spacing multiplier over time to make obstacles spawn farther apart 
-  obsSpacingMultiplier += 0.001; 
- 
-  let obs = { 
-    x: board.width + Math.floor(Math.random() * obsSpacing), 
-    y: board.height - obsSize, 
-    size: obsSize, 
-    color: '#333', 
-  }; 
- 
-  obstacles.push(obs); 
-} 
- 
-function jump() { 
-  if (!isJumping) { 
-    isJumping = true; 
-    jumpVelocity = 10; 
-  } 
-} 
- 
-function gameOver() { 
-  alert('Game Over! Your score: ' + score); 
-  // Optionally reset the game state 
-  obstacles = []; 
-  score = 0; 
+let board, context;
+let obsSize = 15,
+  minObstacleSpacing = 700,
+  maxObstacleSpacing = 2000,
+  obstacleSpeed = 0.5,
+  lastObstacleSpawnTime = 0;
+
+let dino = { x: 50, y: 180, width: 20, height: 20, color: '#666' };
+let obstacles = [];
+let gravity = 0.8, isJumping = false, jumpVelocity;
+let spawnChance = 0.02, score = 0;
+
+window.onload = function () {
+  board = document.getElementById('board');
+  context = board.getContext('2d');
+  board.width = 600;
+  board.height = 200;
+
+  document.addEventListener('keydown', jump);
+
+  requestAnimationFrame(animate);
+};
+
+function animate() {
+  update();
+  requestAnimationFrame(animate);
+}
+
+function update() {
+  context.clearRect(0, 0, board.width, board.height);
+
+  spawnChance += 0.001;
+  if (Math.random() < spawnChance) spawnObstacle();
+
+  handleJump();
+
+  moveAndDrawObstacles();
+
+  detectCollisions();
+
+  drawDino();
+  drawScore();
+  score++;
+}
+
+function spawnObstacle() {
+  let currentTime = Date.now();
+  let elapsedTime = currentTime - lastObstacleSpawnTime;
+
+  if (elapsedTime > minObstacleSpacing) {
+    lastObstacleSpawnTime = currentTime;
+
+    let obsSpacing = Math.floor(Math.random() * (maxObstacleSpacing - minObstacleSpacing + 20) + minObstacleSpacing);
+
+    obstacles.push({ x: board.width, y: board.height - obsSize, size: obsSize, color: '#333' });
+  }
+
+  obstacleSpeed += 0.001;
+
+  for (let i = 0; i < obstacles.length; i++) {
+    obstacles[i].x -= obstacleSpeed;
+  }
+}
+
+function handleJump() {
+  if (isJumping) {
+    dino.y -= jumpVelocity;
+    jumpVelocity -= gravity;
+
+    if (dino.y > 180) {
+      isJumping = false;
+      dino.y = 180;
+      jumpVelocity = 0;
+    }
+  }
+}
+
+function moveAndDrawObstacles() {
+  obstacles.forEach((obs) => {
+    obs.x -= 5;
+    context.fillStyle = obs.color;
+    context.globalAlpha = 0.4;
+    context.fillRect(obs.x, obs.y, obs.size, obs.size);
+    context.globalAlpha = 1;
+  });
+}
+
+function detectCollisions() {
+  obstacles.forEach((obs) => {
+    if (obs.x < dino.x + dino.width &&
+      obs.x + obs.size > dino.x &&
+      obs.y < dino.y + dino.height &&
+      obs.y + obs.size > dino.y) {
+      gameOver();
+    }
+  });
+}
+
+function drawDino() {
+  context.fillStyle = dino.color;
+  context.fillRect(dino.x, dino.y, dino.width, dino.height);
+}
+
+function drawScore() {
+  context.fillStyle = '#000';
+  context.font = '20px Arial';
+  context.fillText('Score: ' + score, 50, 50);
+}
+
+function jump() {
+  if (!isJumping) {
+    isJumping = true;
+    jumpVelocity = 10;
+  }
+}
+
+function gameOver() {
+  alert('Game Over! Your score: ' + score);
+  obstacles = [];
+  score = 0;
 }
